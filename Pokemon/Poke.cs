06459a6bbe5277c5134003d6c9222f7cbc1a36ef
Sujal Gupta <sujalgvs987@gmail.com>
2023-03-11 02:27:17 +0530

    Add project files.

---
+++ Pokemon/Poke.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    class database
+    {
+        public List<string> pokelist;
+        public Dictionary<string,string> pokeimg;
+        public database(){
+            this.pokelist = new List<string>() { "Pikachu","Squirtle","Jigglypiff"};
+            this.pokeimg = new Dictionary<string, string>();
+            
+            pokeimg.Add(pokelist[0],
+                @"
+.__                           __.
+   \ `\~~---..---~~~~~~--.---~~| /   
+    `~-.   `                   .~         _____ 
+        ~.                .--~~    .---~~~    /
+         / .-.      .-.      |  <~~        __/
+        |  |_|      |_|       \  \     .--'
+       /-.      -       .-.    |  \_   \_
+       \-'   -..-..-    `-'    |    \__  \_ 
+        `.                     |     _/  _/
+          ~-                .,-\   _/  _/
+         /                 -~~~~\ /_  /_
+        |               /   |    \  \_  \_ 
+        |   /          /   /      | _/  _/
+        |  |          |   /    .,-|/  _/ 
+        )__/           \_/    -~~~| _/
+          \                      /  \
+           |           |        /_---` 
+           \    .______|      ./
+           (   /        \    /        
+           `--'          /__/");
+            pokeimg.Add(pokelist[1],
+                @"
+           ___
+      _.-~~   ~~~-.
+     /         _   ~.
+    |#`       /#`    \
+    |-'|      |-'|    | 
+    /--        --     |-.
+    \__   .  .        / /\_  
+     \ ~~--___---~~/\| |   ~-.
+  .---`~~--____---_)  \ \-__  \
+  ) <    |__    __\_   \ \     |
+  ~-.__ /   ~~~~   \   \ \     |
+       ~-.   |     .~-.-' |    |
+        | \___|___/     / /     | 
+        | /   |   \     | |  /  |
+         \     |   ~-___ \ \/  /
+         /\__ / `._ /   ~-\ \_/
+        /    \_____|      |`~
+       |      |    |      |   
+        \     |    |      |
+         >______)   /_/\/\_\");
+            pokeimg.Add(pokelist[2],
+                @"
+      --_                       _--
+     ||\_\__                 __/_/||
+     ||  \  \__...-----...__/  /  ||
+     \\  _\_/    /     \    \_/_  //
+      |_/       |       |       \_| 
+      /    __   /     _/    __    \
+     /  .-~--~-.\    / ) .-~--~-.  \
+    |   ||    || \____/  ||    ||   |
+    |   `-_--_-'         `-_--_-'   |
+    |      ~~               ~~      | 
+    |              ___              |
+    |              `-'              |
+     \ \_  \                /  _/  /         
+      \_ \_/                \_/  _/ 
+        \___                 ___/   Amw
+        ..--\__           __/--..
+      .~    __-~~~-----~~~-__    ~.
+     (__.--~                 ~--.__)");
+        
+        
+        
+        
+        }
+
+        public string randp()
+        {
+            Random rand = new Random();
+            return pokelist[rand.Next(pokelist.Count)];
+        }
+    }
+    class Poke
+    {
+        public string name;
+        public string type;
+        public int age;
+        public Poke(string type)
+        {
+            this.type = type;
+            var rand = new Random();
+            this.age = rand.Next(10);
+        }
+        public void askname()
+        {
+            Console.WriteLine("What would you like to name your pokemon?");
+            this.name = Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Pokemon/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static System.Console;
+
+namespace Pokemon
+{
+    class Program
+    {
+        public static void menu()
+        {
+            database db = new database();
+            int opt = 0;
+            while (opt != 5)
+            {
+                WriteLine("What would you like to do now?");
+                WriteLine("1. Look around\n2. Move south\n3. Move north\n4. Move west\n4. Move east\n5. Exit");
+                opt = Convert.ToInt32(ReadLine());
+                if (opt == 1)
+                {
+                    WriteLine("Looking..."); Thread.Sleep(500);
+                    WriteLine("Looking..."); Thread.Sleep(500);
+                    WriteLine("Looking..."); Thread.Sleep(500);
+                    string pfound = db.randp();
+                    WriteLine(db.pokeimg[pfound]);
+                    WriteLine($"Woohoo! You found a {pfound}");
+
+
+                }
+                else if (opt == 2)
+                {
+
+                }
+                else if (opt == 3) { }
+                else if (opt == 4) { }
+                else if (opt == 5) { WriteLine("Thank you for playing!"); break; }
+                else break;
+            }
+        }
+        static void Main(string[] args)
+        { 
+            Title = "Best Pokemon game of all time";
+            database db = new database();
+            string title = @"
+                                  ,'\
+    _.----.        ____         ,'  _\   ___    ___     ____
+_,-'       `.     |    |  /`.   \,-'    |   \  /   |   |    \  |`.
+\      __    \    '-.  | /   `.  ___    |    \/    |   '-.   \ |  |
+ \.    \ \   |  __  |  |/    ,','_  `.  |          | __  |    \|  |
+   \    \/   /,' _`.|      ,' / / / /   |          ,' _`.|     |  |
+    \     ,-'/  /   \    ,'   | \/ / ,`.|         /  /   \  |     |
+     \    \ |   \_/  |   `-.  \    `'  /|  |    ||   \_/  | |\    |
+      \    \ \      /       `-.`.___,-' |  |\  /| \      /  | |   |
+       \    \ `.__,'|  |`-._    `|      |__| \/ |  `.__,'|  | |   |
+        \_.-'       |__|    `-._ |              '-.|     '-.| |   |
+                                `'                            '-._|";
+            WriteLine(title);
+            WriteLine("\nWelcome new player!");
+            WriteLine("What is your name?");
+            string name = ReadLine();
+            WriteLine($"Let's start, {name}!");
+            menu();
+
+
+
+
+
+            WriteLine("\nPress any key to exit...");
+            ReadKey();
+
+        }
+    }
+}
